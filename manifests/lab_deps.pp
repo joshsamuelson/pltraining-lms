5d072eef01a4ee58681abffe628ab77a29a337e8
Josh Samuelson <js@puppetlabs.com>
2015-04-14 14:59:50 -0700

    Copy files from courseware repo

--- manifests/lab_deps.pp
+++ manifests/lab_deps.pp
@@ -3,4 +3,25 @@ class lms::lab_deps{
     ensure   => present,
     provider => gem,
   }
+
+  # Clone the courseware and copy example files to appropriate places 
+  vcsrepo { '/usr/src/courseware-lms-content':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/puppetlabs/courseware-lms-content.git'
+  }
+  exec { 'copy modules':
+    command => 'cp -r /usr/src/courseware-lms-content/modules/* /etc/puppetlabs/puppet/modules/',
+    require => Vcsrepo['/usr/src/courseware-lms-content'],
+  }
+  exec { 'copy hieradata':
+    command => 'cp -r /usr/src/courseware-lms-content/hiera/hieradata/ /etc/puppetlabs/puppet/',
+    require => Vcsrepo['/usr/src/courseware-lms-content'],
+  }
+  exec { 'copy hiera.yaml':
+    command => 'yes | cp /usr/src/courseware-lms-content/hiera/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml'
+    require => Vcsrepo['/usr/src/courseware-lms-content'],
+  }
+
+
 }

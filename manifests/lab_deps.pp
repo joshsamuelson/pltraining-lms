06e7930687960bc6d103ef2f93a255d0ea35f809
Josh Samuelson <js@puppetlabs.com>
2015-05-01 12:48:22 -0700

    Updated path

--- manifests/lab_deps.pp
+++ manifests/lab_deps.pp
@@ -12,14 +12,17 @@ class lms::lab_deps{
   }
   exec { 'copy modules':
     command => 'cp -r /usr/src/courseware-lms-content/modules/* /etc/puppetlabs/puppet/modules/',
+    path    => '/bin/:/usr/bin',
     require => Vcsrepo['/usr/src/courseware-lms-content'],
   }
   exec { 'copy hieradata':
     command => 'cp -r /usr/src/courseware-lms-content/hiera/hieradata/ /etc/puppetlabs/puppet/',
+    path    => '/bin/:/usr/bin',
     require => Vcsrepo['/usr/src/courseware-lms-content'],
   }
   exec { 'copy hiera.yaml':
     command => 'yes | cp /usr/src/courseware-lms-content/hiera/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml',
+    path    => '/bin/:/usr/bin',
     require => Vcsrepo['/usr/src/courseware-lms-content'],
   }
 

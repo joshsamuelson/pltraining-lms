67a2698c50484a1c3584030d8d090ec6c65fedad
Josh Samuelson <js@puppetlabs.com>
2015-06-24 12:05:48 -0700

    Splitting up classes for lab deps

---
+++ manifests/course_selector.pp
@@ -0,0 +1,8 @@
+class lms::course_selector{
+  file { '/usr/local/bin/course_selector':
+    ensure => present,
+    mode   => 755,
+    source => '/usr/src/courseware-lms-content/scripts/course_selector.rb',
+    require => Vcsrepo['/usr/src/courseware-lms-content'],
+  }
+}

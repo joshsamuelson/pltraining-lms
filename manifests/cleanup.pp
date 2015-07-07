4d122e6ed337e4cba0e0c4d33b55ac93752bba63
Josh Samuelson <js@puppetlabs.com>
2015-07-07 15:01:18 -0700

    Removing unused files

--- manifests/cleanup.pp
+++
@@ -1,8 +0,0 @@
-class lms::cleanup {  
-  # /etc/puppet/ssl is confusing to have around. Sloppy. Kill.
-  file {'/etc/puppet':
-    ensure  => absent,
-    recurse => true,
-    force   => true,
-  }
-}

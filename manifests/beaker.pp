6ce3704ea41ef52d7d2585fec7f3ec7785bf4922
Josh Samuelson <js@puppetlabs.com>
2015-07-29 16:42:48 -0700

    typo

--- manifests/beaker.pp
+++ manifests/beaker.pp
@@ -4,7 +4,7 @@ class lms::beaker {
     ensure   => present,
     provider => 'gem',
   }
-  package {['rubygem-nokogiri','gcc-c++]:
+  package {['rubygem-nokogiri','gcc-c++']:
     ensure => present,
     before => Package['beaker']
   }

62215e309a90f724673dabe75315d9a7dbe39a6b
Josh Samuelson <js@puppetlabs.com>
2015-07-29 15:41:20 -0700

    Beaker/docker prereqs

---
+++ manifests/beaker.pp
@@ -0,0 +1,11 @@
+class lms::beaker {
+  include docker
+  package {'beaker':
+    ensure   => present,
+    provider => 'gem',
+  }
+  package {['zlib-devel','gcc-c++','libstdc++-devel']:
+    ensure => present,
+    before => Package['beaker']
+  }
+}

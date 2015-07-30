27f1b4f89e4abf29d1db58e7afea98da30e9fbc0
Josh Samuelson <js@puppetlabs.com>
2015-07-30 11:04:19 -0700

    Updating paths for 2015.2

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -1,13 +1,13 @@
 # Add default memory settings after PE install
 
 class lms::defaults {
-  file { '/etc/puppetlabs/puppet/hieradata':
+  file { '/etc/puppetlabs/code/hieradata':
     ensure => directory,
     require => Class['lms::install_pe'],
   }
-  file { '/etc/puppetlabs/puppet/hieradata/defaults.yaml':
+  file { '/etc/puppetlabs/code/hieradata/defaults.yaml':
     ensure => present,
     source => 'puppet:///modules/bootstrap/lms/defaults.yaml',
-    require => File['/etc/puppetlabs/puppet/hieradata'],
+    require => File['/etc/puppetlabs/code/hieradata'],
   }
 }
